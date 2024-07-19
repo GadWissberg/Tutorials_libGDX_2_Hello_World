4b38dcac4251662d899c0072962a915605b74402
Gad Wissberg <gadw17@gmail.com>
2024-07-19 17:27:07 +0300

    - Changed to use lazy. - Added .gitignore.

--- core/src/main/kotlin/com/gadwissberg/example/LibktxHelloWorld.kt
+++ core/src/main/kotlin/com/gadwissberg/example/LibktxHelloWorld.kt
@@ -9,21 +9,19 @@ import com.badlogic.gdx.math.Vector2
 
 class LibktxHelloWorld : ApplicationAdapter() {
 
-    private var batch: SpriteBatch? = null
-    private var image: Texture? = null
+    private val batch: SpriteBatch by lazy { SpriteBatch() }
+    private val image: Texture by lazy { Texture("smiley.png") }
     private val position: Vector2 = Vector2(140f, 210f)
     private val velocity: Vector2 = Vector2(1F, 0F)
 
     override fun create() {
-        batch = SpriteBatch()
-        image = Texture("smiley.png")
     }
 
     override fun render() {
         position.add(velocity)
 
 
-        if (position.x <= 0F || position.x + image!!.width >= Gdx.graphics.width) {
+        if (position.x <= 0F || position.x + image.width >= Gdx.graphics.width) {
             velocity.scl(-1F)
         }
 
@@ -32,13 +30,13 @@ class LibktxHelloWorld : ApplicationAdapter() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
 
-        batch!!.begin()
-        batch!!.draw(image, position.x, position.y)
-        batch!!.end()
+        batch.begin()
+        batch.draw(image, position.x, position.y)
+        batch.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        image!!.dispose()
+        batch.dispose()
+        image.dispose()
     }
 }
